5d7427eb72583a004b251fc33f1d6c897e8622f8
natashalee <tashalee168@gmail.com>
2018-04-01 21:39:58 -0700

    initial code

---
+++ Nuno/Nuno/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nuno
+//
+//  Created by Natasha Lee on 3/31/18.
+//  Copyright © 2018 retashi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
